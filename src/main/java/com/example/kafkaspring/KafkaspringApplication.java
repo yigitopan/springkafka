1a5cd8654ef8e59c4c05505c383a3260284c1db4
yigit.opan <yigit.opan@mdpgroup.com>
2024-04-28 15:03:27 +0300

    first commit kafka

---
+++ src/main/java/com/example/kafkaspring/KafkaspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaspringApplication.class, args);
+    }
+
+}
